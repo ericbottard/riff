e0eba6ef1f572f14dcc2f8dc1d2b85f49330b83b
Eric Bottard <eric.bottard+github@gmail.com>
2018-05-09 13:35:58 +0200

    Add windowing capability at sidecar
    The sidecar is now able to slice the incoming input data in "windows"
    and perform a different gRPC Call() for each window.
    
    Window boundaries can be specified according to several strategies.
    This PR provides three:
    - no windowing at all
    - (processing) time based
    - number of messages
    
    The default is no windowing at all
    
    Fixes #574
    
    riff-cli sets default windowing size to 1

--- function-sidecar/pkg/dispatcher/grpc/grpc.go
+++ function-sidecar/pkg/dispatcher/grpc/grpc.go
@@ -31,13 +31,20 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"encoding/json"
+	"os"
+	"github.com/projectriff/riff/kubernetes-crds/pkg/apis/projectriff.io/v1alpha1"
 )
 
 type grpcDispatcher struct {
-	stream function.MessageFunction_CallClient
-	input  chan message.Message
-	output chan message.Message
-	closed chan struct{}
+	stream           function.MessageFunction_CallClient
+	client           function.MessageFunctionClient
+	input            chan message.Message
+	output           chan message.Message
+	closed           chan struct{}
+	correlation      CorrelationStrategy
+	windowingFactory WindowingStrategyFactory
+	streams          map[interface{}]window
 }
 
 func (this *grpcDispatcher) Input() chan<- message.Message {
@@ -52,44 +59,100 @@ func (this *grpcDispatcher) Closed() <-chan struct{} {
 	return this.closed
 }
 
-func (this *grpcDispatcher) handleIncoming() {
-	for {
-		select {
-		case in, open := <-this.input:
-			if open {
-				grpcMessage := toGRPC(in)
-				err := this.stream.Send(grpcMessage)
-				if err != nil {
-					if streamClosureDiagnosed(err) {
-						close(this.closed)
-						return
-					}
+// A CorrelationStrategy groups together messages that belong to the same "spatial" stream (the "temporal" aspect
+// of grouping is taken care of by a WindowStrategy). Several invocations for different spatial groups may happen concurrently.
+type CorrelationStrategy func(message.Message) interface{}
 
-					log.Printf("Error sending message to function: %v", err)
-				}
-			} else {
-				close(this.output)
-				log.Print("Shutting down gRPC dispatcher")
-				return
-			}
-		}
-	}
+// A WindowingStrategyFactory is a function for creating a new instance of a (most certainly stateful) WindowingStrategy,
+// given a new, first message for a stream.
+type WindowingStrategyFactory func(message.Message) WindowingStrategy
+
+//
+type WindowingStrategy interface {
+	// ShouldClose notifies this strategy that a new message has just been sent to the function, resulting in the optional
+	// error passed as 2nd argument.
+	// ShouldClose should return true as its first result if this strategy decides that the current stream should end.
+	ShouldClose(in message.Message, err error) bool
 }
 
-func (this *grpcDispatcher) handleOutgoing() {
+const correlationId = "correlationId"
+var headersToForward []string
+func init() {
+	headersToForward = []string {correlationId}
+}
+
+
+type window struct {
+	stream           function.MessageFunction_CallClient
+	release          WindowingStrategy
+	forwardedHeaders message.Headers
+}
+
+func (d *grpcDispatcher) handleIncoming() {
+	for in := range d.input {
+		// TODO: problem: requires one incoming message to trigger a Call(), what about Supplier functions?
+		key := d.correlation(in)
+		w, ok := d.streams[key]
+		if !ok {
+			log.Printf("Opening new Call()\n")
+			stream, err := d.client.Call(context.Background())
+			if err == nil {
+				w.stream = stream
+				w.release = d.windowingFactory(in)
+				w.forwardedHeaders = retainHeaders(in)
+				go w.handleOutgoing(d.output)
+			} else {
+				// TODO
+				log.Printf("Error in open %v\n", err)
+			}
+			d.streams[key] = w
+		} else {
+			log.Printf("Re-using existing Call()\n")
+		}
+
+		grpcMessage := toGRPC(in)
+		err := w.stream.Send(grpcMessage)
+
+		if w.release.ShouldClose(in, err) {
+			log.Printf("End of stream, closing\n")
+			if err := w.stream.CloseSend(); err != nil {
+				// TODO
+				log.Printf("Error in close %v\n", err)
+			}
+			delete(d.streams, key)
+		} else {
+			log.Printf("Keeping stream open\n")
+		}
+	}
+	fmt.Printf("Done\n")
+}
+
+func retainHeaders(in message.Message) message.Headers {
+	result := make(message.Headers)
+	for _, h := range headersToForward {
+		if v, ok := in.Headers()[h] ; ok {
+			result[h] = v
+		}
+	}
+	return result
+}
+
+func (this *window) handleOutgoing(output chan<- message.Message) {
 	for {
 		reply, err := this.stream.Recv()
-		if err != nil {
-			if streamClosureDiagnosed(err) {
-				close(this.closed)
-				return
-			}
-
+		if err == io.EOF {
+			break
+		} else if err != nil {
 			log.Printf("Error receiving message from function: %v", err)
-			continue
+			break
 		}
 		message := toDispatcher(reply)
-		this.output <- message
+		for h, v := range this.forwardedHeaders {
+			if _, ok := message.Headers()[h] ; !ok {
+				message.Headers()[h] = v
+			}
+		}
+		output <- message
 	}
 }
 
@@ -114,14 +177,23 @@ func NewGrpcDispatcher(port int, timeout time.Duration) (dispatcher.Dispatcher, 
 		return nil, err
 	}
 
-	fnStream, err := function.NewMessageFunctionClient(conn).Call(context.Background())
-	if err != nil {
-		return nil, err
+	result := &grpcDispatcher{
+		client:         function.NewMessageFunctionClient(conn),
+		input:          make(chan message.Message, 100),
+		output:         make(chan message.Message, 100),
+		closed:         make(chan struct{}),
+		streams:        make(map[interface{}]window),
+		windowingFactory: unmarshallFactory(),
+		correlation: func(m message.Message) interface{} {
+			// Correlate by correlationId header by default if present
+			if c, ok := m.Headers()[correlationId] ; ok {
+				return c[0]
+			} else {
+				return nil
+			}
+		},
 	}
-
-	result := &grpcDispatcher{fnStream, make(chan message.Message, 100), make(chan message.Message, 100), make(chan struct{})}
 	go result.handleIncoming()
-	go result.handleOutgoing()
 
 	return result, nil
 }
@@ -146,4 +218,29 @@ func toDispatcher(grpc *function.Message) message.Message {
 		dHeaders[k] = pv.Values
 	}
 	return message.NewMessage(grpc.Payload, dHeaders)
+}
+
+func unmarshallFactory() WindowingStrategyFactory {
+	var w v1alpha1.Windowing
+	json.Unmarshal([]byte(os.Getenv("WINDOWING_STRATEGY")), &w)
+	// TODO: this can be done reflectively, once there are more alternatives.
+
+	if w.Size != int32(0) && w.Time != "" {
+		panic("windowing by time and size are mutually exclusive")
+	}
+
+	if w.Size != int32(0) {
+		log.Printf("Will use windowing strategy of %v messages\n", w.Size)
+		return sizeFactoryFactory(int(w.Size))
+	} else if w.Time != "" {
+		log.Printf("Will use time based windowing strategy of %v\n", w.Time)
+		d, err := time.ParseDuration(w.Time)
+		if err != nil {
+			panic(err) // Assume it's validated at fn registration time
+		}
+		return ptimeFactoryFactory(d)
+	} else {
+		log.Printf("Will use no windowing strategy (unbounded stream)\n")
+		return noneFactoryFactory()
+	}
 }
